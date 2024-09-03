53a09ba03bea65f9d158c1233c785ecc8e1b3c87
Shambhavi Singh <88976526+Shamvv@users.noreply.github.com>
2024-09-03 23:47:32 +0530

    Time: 41 ms (35.09%), Space: 16.4 MB (93.62%) - LeetHub

--- 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.py
+++ 2076-sum-of-digits-of-string-after-convert/2076-sum-of-digits-of-string-after-convert.py
@@ -5,13 +5,10 @@ class Solution:
         res = 0
         for i in s:
             conv += str(a.index(i) + 1)
-        print(conv)
-
         while k > 0:
             res = 0
             for i in conv:
                 res += int(i)
             conv = str(res)
             k -= 1
-            print(k, res, conv)
         return res
