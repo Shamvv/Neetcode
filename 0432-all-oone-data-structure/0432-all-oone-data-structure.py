0b0cc94a8c5e81abea73544e5bf2467fe053f131
Shambhavi Singh <88976526+Shamvv@users.noreply.github.com>
2024-09-29 21:55:11 +0530

    Time: 143 ms (90.47%), Space: 31.9 MB (89.85%) - LeetHub

--- 0432-all-oone-data-structure/0432-all-oone-data-structure.py
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.py
@@ -1,32 +1,68 @@
 class AllOne:
+
     def __init__(self):
-        self.myDict = {}
+       
+        self.d={}
+       
+        self.op=0
+       
+        self.res=""
 
     def inc(self, key: str) -> None:
-        if key in self.myDict:
-            self.myDict[key] += 1
+      
+        self.op=0
+        if (key in self.d):
+            self.d[key]+=1
         else:
-            self.myDict[key] = 1
-
+            self.d[key]=1
+        return None
     def dec(self, key: str) -> None:
-        if key in self.myDict:
-            if self.myDict[key] > 1:
-                self.myDict[key] -= 1
-            else:
-                self.myDict.pop(key)
+     
+        self.op=0
+        if (self.d[key]>1):
+            self.d[key]-=1
+        else:
+            del self.d[key]
+        return None
 
     def getMaxKey(self) -> str:
-        if not self.myDict:
-            return ""
-        maxVal = max(self.myDict.values())
-        for key in self.myDict.keys():
-            if self.myDict[key] == maxVal:
-                return key
+       
+        if (self.op==1):
+            return self.res
+        self.op=1
+        if (self.d):
+            a=list(self.d.values())[0]
+            b=list(self.d.keys())[0]
+            for i in self.d.keys():
+                if (self.d[i]>a):
+                    a=self.d[i]
+                    b=i
+            self.res=b
+            return b
+        self.res=""
+        return ""
 
     def getMinKey(self) -> str:
-        if not self.myDict:
-            return ""
-        minVal = min(self.myDict.values())
-        for key in self.myDict.keys():
-            if self.myDict[key] == minVal:
-                return key
+       
+        if (self.op==2):
+            return self.res
+        self.op=2
+        if (self.d):
+            a=list(self.d.values())[0]
+            b=list(self.d.keys())[0]
+            for i in self.d.keys():
+                if (self.d[i]<a):
+                    a=self.d[i]
+                    b=i
+            self.res=b
+            return b
+        self.res=""
+        return ""
+
+
+# Your AllOne object will be instantiated and called as such:
+# obj = AllOne()
+# obj.inc(key)
+# obj.dec(key)
+# param_3 = obj.getMaxKey()
+# param_4 = obj.getMinKey()
