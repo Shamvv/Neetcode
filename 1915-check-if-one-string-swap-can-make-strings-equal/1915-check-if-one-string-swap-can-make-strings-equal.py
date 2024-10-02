066a3a0ee5442f988ce4116f9122c72420055668
Shambhavi Singh <88976526+Shamvv@users.noreply.github.com>
2024-10-02 09:45:08 +0530

    Time: 37 ms (45.27%), Space: 16.6 MB (6.63%) - LeetHub

--- 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.py
+++ 1915-check-if-one-string-swap-can-make-strings-equal/1915-check-if-one-string-swap-can-make-strings-equal.py
@@ -8,6 +8,6 @@ class Solution:
         for i in range(len(s1)):
             if s1[i] != s2[i]:
                 d.append(i)        
-        if len(d) != 2:
-            return False
-        return (s1[d[0]] == s2[d[1]] and s1[d[1]] == s2[d[0]] )
+        if len(d) == 2:
+            return (s1[d[0]] == s2[d[1]] and s1[d[1]] == s2[d[0]])
+        return False
