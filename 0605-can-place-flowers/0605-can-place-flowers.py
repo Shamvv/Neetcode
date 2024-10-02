d6c201b86281ffb6014ffef7eabcca23c7ff07df
Shambhavi Singh <88976526+Shamvv@users.noreply.github.com>
2024-10-02 09:08:54 +0530

    Time: 139 ms (22.11%), Space: 17 MB (25.15%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -9,7 +9,7 @@ class Solution:
         if f[-1] == 0 and f[-2] == 0:
             f[-1] = 1
             c += 1        
-        for i in range(0, len(f) - 1):
+        for i in range(len(f) - 1):
             if f[i - 1] == 0 and f[i + 1] == 0 and f[i] == 0:
                 f[i] = 1
                 c += 1
