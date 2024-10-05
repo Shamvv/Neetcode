7b1f2faf07e23dca8c446143a2310be04db1f1d2
Shambhavi Singh <88976526+Shamvv@users.noreply.github.com>
2024-10-05 20:35:31 +0530

    Time: 40 ms (99.08%), Space: 16.8 MB (22.1%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -22,3 +22,29 @@ class Solution:
             if count1 == count2:
                 return True
         return False
+        
+        """
+        s11 = [0] * 26
+        s22 = [0] * 26
+
+        for i in s1:
+            s11[ord(i) - 97] += 1            
+
+        for j in s2:
+            s22[ord(j) - 97] += 1
+
+        s1_ = ''.join(str(num) for num in s11)
+        s2_ = ''.join(str(num) for num in s22)
+
+        print(s11, s22)
+        
+        f = 0
+        for i in range(26):
+            if s11[i] == 1:
+                if s11[i] == s22[i]:
+                    continue
+                else:
+                    f = 1
+        
+        return f == 0
+        """
